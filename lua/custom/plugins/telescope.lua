1ff3659b4fdf7be79dc9393568f5a743f69493dc
Jerry Johnson <j.johnson@criteo.com>
2024-06-20 18:28:00 -0400

    keybind for search files -> space space

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -35,7 +35,7 @@ return {
     local builtin = require("telescope.builtin")
     vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[s]earch [h]elp" })
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[s]earch [k]eymaps" })
-    vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[s]earch [f]iles" })
+    vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "search [f]iles" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[s]earch [s]elect Telescope" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[s]earch current [w]ord" })
     vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[s]earch [d]iagnostics" })
